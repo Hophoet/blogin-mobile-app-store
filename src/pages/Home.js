5fc068727ae4b32428db481a41a02d3f26a50c44
hophoet <hophoet@gmail.com>
2021-01-03 15:30:05 +0000

    (components) - update home page screen component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,29 @@
 
 //apk
-import apk from '../apk/file.txt'
-
-import Carousel from '../components/Carousel'
+import '../static/css/home.css'
+// import Carousel from '../components/Carousel'
+import m8 from '../static/images/m4.png'
 
 function App() {
   return (
-    <div className="home__container">
-      <div >
-        <h1>BLOGIN APP</h1>
+    <div className="home">
+      <div className="home__image" >
+        <img className="mockup" src={m8} />
+      </div>
+      <div className="home__info">
+        <h1>BloGin</h1>
+        <p>open source <span>bloging</span> project
+        </p>
+        <div>
+          <ul>
+            <li>Source code : <a target="_blank"  href="hophoet.com">click here</a></li>
+            <li>Developer: <a target="_blank" href='http://www.hophoet.com/'>hophoet</a> </li>
+          </ul>
+        </div>
+        <a className="btn btn-info" href='' >Download now</a>
+        
       </div>
      
-      <Carousel apk={apk}/>
     </div>
   );
 }
